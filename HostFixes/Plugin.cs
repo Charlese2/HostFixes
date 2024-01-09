111baddfb56d97c5ec149f595d819896c1b27d96
Charlese2 <knightside@hotmail.com>
2024-01-09 00:04:14 -0700

    Temporary check to make sure money is spent on the terminal.

--- HostFixes/Plugin.cs
+++ HostFixes/Plugin.cs
@@ -122,7 +122,7 @@ namespace HostFixes
                     return;
                 }
 
-                if (newGroupCredits <= instance.groupCredits)
+                if (clientId != 0 && newGroupCredits < instance.groupCredits)
                 {
                     instance.SyncGroupCreditsServerRpc(newGroupCredits, numItemsInShip);
                 }
@@ -142,7 +142,7 @@ namespace HostFixes
                 }
                 Terminal terminal = FindObjectOfType<Terminal>();
 
-                if (newGroupCreditsAmount <= terminal.groupCredits)
+                if (clientId != 0 && newGroupCreditsAmount < terminal.groupCredits)
                 {
                     StartOfRound.Instance.BuyShipUnlockableServerRpc(unlockableID, newGroupCreditsAmount);
                 }
